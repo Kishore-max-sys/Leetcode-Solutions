255190a9d6c5082ccc874f64ed30081c9ba5c08d
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-08-11 23:11:56 +0530

    Time: 2 ms (29.43%), Space: 43.3 MB (16.81%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,36 +1,31 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int max=Integer.MIN_VALUE;
-        int sum=0;
-        for(int num:nums){
-            max=Math.max(max,num);
-            sum+=num;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+            sum += num;
         }
-        int low=max;
-        int high=sum;
-        int ans=high;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int i=0;
-            int count=1;
-            int s=0;
-            while(i<nums.length){
-                if(count>k){
-                    break;
-                }
-                if(s+nums[i]>mid){
+        int low = max;
+        int high = sum;
+        int ans = high;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int count = 1;
+            int s = 0;
+            for (int num : nums) {
+                if (s + num > mid) {
                     count++;
-                    s=nums[i];
-                }else{
-                    s+=nums[i];
+                    s = num;
+                } else {
+                    s += num;
                 }
-                i++;
             }
-            if(count<=k){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
+            if (count <= k) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
