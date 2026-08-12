26325a15e4fd6ea83ddbd428dd13c564e673fdcd
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-08-13 00:39:41 +0530

    Update 0240-search-a-2d-matrix-ii.java

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -2,15 +2,18 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int row=0;
-        int col=n-1;
-        while(row<m && col>=0){
-            if(matrix[row][col]==target){
-                return true;
-            }else if(matrix[row][col]<target){
-                row++;
-            }else{
-                col--;
+        for(int i=0;i<m;i++){
+            int low=0;
+            int high=n-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(matrix[i][mid]==target){
+                    return true;
+                }else if(matrix[i][mid]<target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
             }
         }
         return false;
