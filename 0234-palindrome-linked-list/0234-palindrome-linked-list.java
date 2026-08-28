e148ef646cc2c60763891829a62f5e9d8c08f22b
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-08-28 23:52:42 +0530

    Update 0234-palindrome-linked-list.java

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -16,21 +16,21 @@ class Solution {
             head2=head2.next;
             fast=fast.next.next;
         }
-        if(fast!=null) head2=head2.next;
         ListNode prev=null;
-        while(head2!=null){
-            ListNode next=head2.next;
-            head2.next=prev;
-            prev=head2;
-            head2=next;
+        ListNode temp=head;
+        while(temp!=null&&temp!=head2){
+            ListNode next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
         }
-        ListNode head1=head;
-        head2=prev;
-        while(head1!=null&&head2!=null){
-            if(head1.val!=head2.val){
+        ListNode tail1=prev;
+        if(fast!=null) head2=head2.next;
+        while(tail1!=null&&head2!=null){
+            if(tail1.val!=head2.val){
                 return false;
             }
-            head1=head1.next;
+            tail1=tail1.next;
             head2=head2.next;
         }
         return true;
