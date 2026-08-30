3d23667dcc64063e6e12e31483c4dc90b6798c57
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-08-31 00:25:37 +0530

    Update 0160-intersection-of-two-linked-lists.java

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,31 +11,14 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int n1=0;
         ListNode tempA=headA;
         while(tempA!=null){
-            n1++;
+            ListNode tempB=headB;
+            while(tempB!=null&&tempB!=tempA){
+                tempB=tempB.next;
+            }
+            if(tempB!=null) return tempB;
             tempA=tempA.next;
-        }
-        int n2=0;
-        ListNode tempB=headB;
-        while(tempB!=null){
-            n2++;
-            tempB=tempB.next;
-        }
-        if(n1>n2) return getIntersectionNode(headB,headA);
-        tempA=headA;
-        tempB=headB;
-        while(n2>=n1&&tempB!=null){
-            if(tempA==tempB) return tempA;
-            tempB=tempB.next;
-            n2--;
-        }
-        tempA=tempA.next;
-        while(tempA!=null&&tempB!=null){
-            if(tempA==tempB) return tempA;
-            tempA=tempA.next;
-            tempB=tempB.next;
         }
         return null;
     }
