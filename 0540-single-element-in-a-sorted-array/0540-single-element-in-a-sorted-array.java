bd2f24b00fb901ceea8031bf85b6f3b94d2770ad
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-09-05 00:10:18 +0530

    Time: 0 ms (100%), Space: 52.8 MB (58.58%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,13 +4,16 @@ class Solution {
         int low=0;
         int high=n-1;
         while(low<=high){
-            if(low<n-1 && nums[low]!=nums[low+1]){
-                return nums[low];
-            }else if(high>0 && nums[high]!=nums[high-1]){
-                return nums[high];
+            int mid=low+(high-low)/2;
+            if(mid%2==1&&mid!=n-1){
+                mid++;
+            }
+            if(mid<n-1&&nums[mid]==nums[mid+1]){
+                low=mid+2;
+            }else if(mid>0&&nums[mid]==nums[mid-1]){
+                high=mid-2;
             }else{
-                low+=2;
-                high-=2;
+                return nums[mid];
             }
         }
         return nums[0];
