b4509a0b309c3f3818ef230ff8eeb3bbb7a6c662
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-09-06 15:11:06 +0530

    Time: 6 ms (99.73%), Space: 48 MB (43.47%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -25,7 +25,7 @@ class Solution {
     long calTime(int[] arr,int ele){
         long total=0;
         for(int i=0;i<arr.length;i++){
-            total+=(int)Math.ceil((double)arr[i]/ele);
+            total+=(arr[i]+ele-1)/ele;
         }
         return total;
     }
