0b9e4681ce2567e81c3c548cec726ce310d57054
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-09-07 11:25:54 +0530

    Time: 0 ms (100%), Space: 44.1 MB (8.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -4,24 +4,22 @@ class Solution {
         int n=matrix[0].length;
         int lr=0;
         int hr=m-1;
-        int row=-1;
         while(lr<=hr){
             int mid=(lr+hr)/2;
             if(matrix[mid][0]<=target){
-                row=mid;
                 lr=mid+1;
             }else{
                 hr=mid-1;
             }
         }
-        if(row==-1) return false;
+        if(hr==-1) return false;
         int low=0;
         int high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(matrix[row][mid]==target){
+            if(matrix[hr][mid]==target){
                 return true;
-            }else if(matrix[row][mid]<target){
+            }else if(matrix[hr][mid]<target){
                 low=mid+1;
             }else{
                 high=mid-1;
