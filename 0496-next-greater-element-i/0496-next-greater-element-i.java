fba0f6171a70b887cbb7108cc83cf213bde6b78e
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-09-10 17:48:38 +0530

    Time: 2 ms (99.52%), Space: 45.4 MB (35.62%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(nums2[nums2.length-1],-1);
-        for(int i=nums2.length-2;i>=0;i--){
-            if(nums2[i]<nums2[i+1]){
-                map.put(nums2[i],nums2[i+1]);
-            }else{
-                int temp=nums2[i+1];
-                while(map.get(temp)!=-1&&nums2[i]>=map.get(temp)){
-                    temp=map.get(temp);
-                }
-                map.put(nums2[i],map.get(temp));
+        Stack<Integer> st=new Stack<>();
+        int[] nge=new int[10001];
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty()&&nums2[i]>=st.peek()){
+                st.pop();
             }
+            if(st.isEmpty()) nge[nums2[i]]=-1;
+            else nge[nums2[i]]=st.peek();
+            st.push(nums2[i]);
         }
         int[] ans=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            ans[i]=map.get(nums1[i]);
+            ans[i]=nge[nums1[i]];
         }
         return ans;
     }
