a294693ecba2c08bfd2d932a71e131ac45bae7f1
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-09-11 16:44:12 +0530

    Time: 5 ms (65.03%), Space: 47 MB (55.39%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -2,28 +2,19 @@ class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         int n=asteroids.length;
         Stack<Integer> st=new Stack<>();
-        for(int i=0;i<n;i++){
-            if(asteroids[i]<0){
-                boolean isAlive=true;
-                while(!st.isEmpty()&&st.peek()>0){
-                    int big=Math.max(st.peek(),Math.abs(asteroids[i]));
-                    if(big==st.peek()){
-                        if(big==Math.abs(asteroids[i])){
-                            st.pop();
-                        }
-                        isAlive=false;
-                        break;
-                    }
-                    else{
-                        st.pop();
-                    }
+        for(int a:asteroids){
+            boolean isAlive=true;
+            while(!st.isEmpty()&&st.peek()>0&&a<0&&isAlive){
+                if(st.peek()<-a){
+                    st.pop();
+                }else if(st.peek()==-a){
+                    st.pop();
+                    isAlive=false;
+                }else{
+                    isAlive=false;
                 }
-                if(isAlive){
-                    st.push(asteroids[i]);
-                }
-            }else{
-                st.push(asteroids[i]);
             }
+            if(isAlive) st.push(a);
         }
         int[] ans=new int[st.size()];
         int i=st.size()-1;
