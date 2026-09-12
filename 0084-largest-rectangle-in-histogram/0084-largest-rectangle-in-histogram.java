5852e70b1b31d2855f286f78e662e34f9ab52f88
Kishore-max-sys <seerlayuvasrinivaskishore@gmail.com>
2026-09-12 20:28:46 +0530

    Time: 72 ms (37.9%), Space: 78.2 MB (50.7%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,43 +1,27 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n=heights.length;
-        int[] nse=findNSE(heights);
-        int[] pse=findPSE(heights);
         long area=0;
+        Stack<Integer> st=new Stack<>();
         for(int i=0;i<n;i++){
-            int left=pse[i];
-            int right=nse[i];
+            while(!st.isEmpty()&&heights[st.peek()]>heights[i]){
+                int ele=heights[st.peek()];
+                st.pop();
+                int right=i;
+                int left=st.isEmpty()?-1:st.peek();
+                int width=right-left-1;
+                area=Math.max(area,(long)ele*width);
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            int right=n;
+            int ele=heights[st.peek()];
+            st.pop();
+            int left=st.isEmpty()?-1:st.peek();
             int width=right-left-1;
-            area=Math.max(area,(long)heights[i]*width);
+            area=Math.max(area,(long)ele*width);
         }
         return (int)area;
-    }
-    int[] findNSE(int[] arr){
-        int n=arr.length;
-        int[] nse=new int[n];
-        Stack<Integer> st=new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty()&&arr[i]<=arr[st.peek()]){
-                st.pop();
-            }
-            if(st.isEmpty()) nse[i]=n;
-            else nse[i]=st.peek();
-            st.push(i);
-        }
-        return nse;
-    }
-    int[] findPSE(int[] arr){
-        int n=arr.length;
-        int[] pse=new int[n];
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty()&&arr[i]<=arr[st.peek()]){
-                st.pop();
-            }
-            if(st.isEmpty()) pse[i]=-1;
-            else pse[i]=st.peek();
-            st.push(i);
-        }
-        return pse;
     }
 }
